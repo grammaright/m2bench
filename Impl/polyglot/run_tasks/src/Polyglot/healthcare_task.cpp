be84ce0d2faf9b58e3c31b404fd0131d122d25b3
Kyoseung Koo <koo@dbs.snu.ac.kr>
2025-05-26 15:46:09 +0900

    T9

--- Impl/polyglot/run_tasks/src/Polyglot/healthcare_task.cpp
+++ Impl/polyglot/run_tasks/src/Polyglot/healthcare_task.cpp
@@ -1,14 +1,12 @@
-#include <time.h>
+#include "Connection/Connection.h"
+#include "Polyglot/func.h"
 
-#include <chrono>
+// prevision
+#include "interface/functions.h"
+#include "type/type.h"
 
 using namespace std;
-using std::chrono::duration;
-using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
-using std::chrono::milliseconds;
-
-#define BUFFER 1000
+using namespace duckdb;
 
 /**
 
@@ -58,175 +56,129 @@ using std::chrono::milliseconds;
  *
  */
 
-void T9(int patient_id) {
-  //   auto mysql = mysql_connector();
+void T9(int patientId) {
+  // const int SF = 1;
+  // const int X = 9 * SF;
+  const int adverseEffectSize = 82853;
+  const int drugSize = 14759;
 
-  //   auto mongodb = mongodb_connector("Healthcare");
-  //   auto db = mongodb.db;
-  //   auto drug = db["drug"];
+  PolyglotConnection conn(true, "healthcare", true);
+  auto &dconn = conn.GetDuckdbConnection();
 
-  //   mongocxx::pipeline p{};
+  // it gives polyglot advantage
+  auto res = dconn.Query(
+      "SELECT doc_get_int32('drug_id', data), "
+      "doc_get_string('adverse_effect_list.adverse_effect_name', data) "
+      "FROM ( "
+      "SELECT doc_insert(data, unnest(doc_get_list('adverse_effect_list', "
+      "data, 1)::VPack[])::VPack, 'adverse_effect_list')::VPack AS data FROM "
+      "Drug) "
+      "GROUP BY doc_get_int32('drug_id', data), "
+      "doc_get_string('adverse_effect_list.adverse_effect_name', data)");
+  dconn.Query(
+      "create temporary table D2A ( "
+      " drug int, "
+      " adverse_effect varchar(100) )");
 
-  //   p.unwind("$adverse_effect_list");
-  //   p.project(make_document(
-  //       kvp("drug", "$drug_id"),
-  //       kvp("adverse_effect", "$adverse_effect_list.adverse_effect_name"),
-  //       kvp("is_adverse_effect", make_document(kvp("$literal", 1)))));
+  Appender d2aAppender(dconn, "D2A");
+  auto resChunk = res->Fetch();
+  while (resChunk) {
+    auto drugIdVector = FlatVector::GetData<int>(resChunk->data[0]);
+    auto nameVector = FlatVector::GetData<string_t>(resChunk->data[1]);
+    for (int i = 0; i < resChunk->size(); ++i) {
+      d2aAppender.BeginRow();
 
-  //   mysql.mysess->sql("use Healthcare").execute();
-  //   mysql.mysess
-  //       ->sql(
-  //           "create temporary table D2A ( "
-  //           " drug int, "
-  //           " adverse_effect varchar(100) )")
-  //       .execute();
+      d2aAppender.Append(drugIdVector[i]);
+      d2aAppender.Append(nameVector[i]);
 
-  //   auto insert2D2A = mysql.mysess->getSchema("Healthcare")
-  //                         .getTable("D2A")
-  //                         .insert("drug", "adverse_effect");
-  //   auto cursor = drug.aggregate(p, mongocxx::options::aggregate{});
-  //   int buffer = 0;
-  //   for (auto row : cursor) {
-  //     int drug_id = row["drug"].get_int32();
-  //     string adverse_effect_id =
-  //     string(row["adverse_effect"].get_utf8().value); int is_adverse_effect =
-  //     row["is_adverse_effect"].get_int32();
+      d2aAppender.EndRow();
+    }
 
-  //     insert2D2A.values(drug_id, adverse_effect_id);
-  //     buffer++;
-  //     if (buffer >= BUFFER) {
-  //       insert2D2A.execute();
-  //       buffer = 0;
-  //       insert2D2A = mysql.mysess->getSchema("Healthcare")
-  //                        .getTable("D2A")
-  //                        .insert("drug", "adverse_effect");
-  //     }
-  //   }
-  //   if (buffer != 0) insert2D2A.execute();
-  //   auto D2A = mysql.mysess->getSchema("Healthcare").getTable("D2A");
+    d2aAppender.Flush();
+    resChunk = res->Fetch();
+  }
 
-  //   mysql.mysess
-  //       ->sql(
-  //           "CREATE TEMPORARY TABLE Rdrug as "
-  //           "(SELECT t.drug, ROW_NUMBER() OVER () -1 as drug_d from "
-  //           "(Select distinct(drug) as drug "
-  //           "from D2A) as t )")
-  //       .execute();
+  // dconn.Query(
+  //     "CREATE TEMPORARY TABLE Rdrug as "
+  //     "(SELECT t.drug, (ROW_NUMBER() OVER () -1)::INTEGER as drug_d from "
+  //     "(Select distinct(drug) as drug "
+  //     "from D2A order by drug) as t )");
+  dconn.Query(
+      "CREATE TEMPORARY TABLE Rdrug as "
+      "(SELECT t.drug, (ROW_NUMBER() OVER () -1)::INTEGER as drug_d from "
+      "(Select distinct(drug) as drug "
+      "from D2A) as t )");
 
-  //   mysql.mysess
-  //       ->sql(
-  //           "CREATE TEMPORARY TABLE Radverse_effect as "
-  //           "(SELECT t.adverse_effect, ROW_NUMBER() OVER () -1 as "
-  //           "adverse_effect_d from "
-  //           "(Select distinct(adverse_effect) as adverse_effect "
-  //           "from D2A) as t )")
-  //       .execute();
+  // dconn.Query(
+  //     "CREATE TEMPORARY TABLE Radverse_effect as "
+  //     "(SELECT t.adverse_effect, (ROW_NUMBER() OVER () - 1)::INTEGER as "
+  //     "adverse_effect_d from "
+  //     "(Select distinct(adverse_effect) as adverse_effect "
+  //     "from D2A order by adverse_effect) as t )");
+  dconn.Query(
+      "CREATE TEMPORARY TABLE Radverse_effect as "
+      "(SELECT t.adverse_effect, (ROW_NUMBER() OVER () - 1)::INTEGER as "
+      "adverse_effect_d from "
+      "(Select distinct(adverse_effect) as adverse_effect "
+      "from D2A) as t )");
 
-  //   mysql.mysess->sql("CREATE INDEX Rdrug on Rdrug(drug)").execute();
-  //   mysql.mysess
-  //       ->sql("CREATE INDEX Radverse_effect on
-  //       Radverse_effect(adverse_effect)") .execute();
+  // dconn.Query("CREATE INDEX Rdrug on Rdrug(drug)");
+  // dconn.Query(
+  //     "CREATE INDEX Radverse_effect on Radverse_effect(adverse_effect)");
 
-  //   auto rows =
-  //       mysql.mysess
-  //           ->sql(
-  //               "SELECT drug_d, adverse_effect_d "
-  //               " From Rdrug, Radverse_effect, D2A "
-  //               " Where D2A.drug = Rdrug.drug "
-  //               " and D2A.adverse_effect = Radverse_effect.adverse_effect ")
-  //           .execute();
+  t9ConstructD(dconn, drugSize, adverseEffectSize);
+  auto D = prevision::OpenArray("__D");
 
-  //   int dim1 =
-  //   mysql.mysess->getSchema("Healthcare").getTable("Rdrug").count(); int dim2
-  //   =
-  //       mysql.mysess->getSchema("Healthcare").getTable("Radverse_effect").count();
-  //   unique_ptr<ScidbConnection> conn(
-  //       new ScidbConnection(SCIDB_HOST_HEALTHCARE + string(":8080")));
+  /* Cosine Similarity */
+  std::vector<uint32_t> tDimOrder = {1, 0};
+  auto E1 = prevision::Matmul(D, prevision::Transpose(D, tDimOrder));
+  auto E2 =
+      prevision::Map(E1, t9_invnorm, {TILESTORE_FLOAT64}, TILESTORE_SPARSE_CSR);
+  auto E = prevision::Matmul(
+      prevision::Transpose(prevision::Matmul(E1, E2), tDimOrder), E2);
 
-  //   conn->exec("remove(temp)");
-  //   conn->exec("remove(drug_matrix)");
-  //   conn->exec("remove(similarity1)");
-  //   conn->exec("remove(similarity2)");
-  //   conn->exec("remove(inv_norm)");
-  //   conn->exec("remove(drug_similarity)");
-  //   conn->exec(
-  //       "create array temp<drug:int64 NOT NULL, adverse_effect:int64 NOT
-  //       NULL, " "is_adverse_effect:double NOT NULL> [i=0:" + to_string(dim1 *
-  //       dim2 - 1) + ":0:1000000]");
-  //   int ncell = 0;
+  auto pvEngine = conn.GetPrevisionEngine();
+  pvEngine->Execute(*E);
 
-  //   ScidbSchema sschema;
-  //   sschema.attrs.push_back(ScidbAttr("drug", INT64));
-  //   sschema.attrs.push_back(ScidbAttr("adverse_effect", INT64));
-  //   sschema.attrs.push_back(ScidbAttr("is_adverse_effect", DOUBLE));
+  auto fRes = dconn.Query(
+      "SELECT DISTINCT Rdrug.drug_d, Rdrug.drug "
+      "FROM Prescription, Rdrug "
+      "WHERE Rdrug.drug = Prescription.drug_id AND Prescription.patient_id = " +
+      to_string(patientId));
+  // auto fRes = dconn.Query(
+  //     "SELECT DISTINCT Rdrug.drug_d, Rdrug.drug "
+  //     "FROM Prescription, Rdrug "
+  //     "WHERE Rdrug.drug = Prescription.drug_id AND Prescription.patient_id =
+  //     " + to_string(patientId) + " ORDER BY Rdrug.drug");
+  auto page = t9GetBuffer(E->getArrayName());
+  auto fResChunk = fRes->Fetch();
+  size_t resCnt = 0;
+  while (fResChunk) {
+    auto drugVec = FlatVector::GetData<int>(fResChunk->data[0]);
+    auto originalDrugIdVec = FlatVector::GetData<int>(fResChunk->data[1]);
+    for (int i = 0; i < fResChunk->size(); ++i) {
+      auto id = drugVec[i];
+      auto oid = originalDrugIdVec[i];
+      auto val = t9GetValues(dconn, page, id);
+      resCnt += val.size();  // not to be eliminated
 
-  //   shared_ptr<ScidbArrFile> coo(new ScidbArrFile(sschema));
+      // For validation
+      // for (auto &item : val) {
+      //   auto a = dconn.Query("SELECT drug FROM Rdrug WHERE drug_d = " +
+      //                        to_string(item.first));
+      //   auto ar = a->Fetch();
+      //   auto av = FlatVector::GetData<int>(ar->data[0]);
+      //   cout << oid << "," << item.first << "," << av[0] << "," <<
+      //   item.second
+      //        << endl;
+      // }
+    }
 
-  //   for (auto row : rows) {
-  //     long long dim_drug = row[0].get<long>();
-  //     long long dim_adverse_effect = row[1].get<long>();
-  //     double is_adverse_effect = 1.0;
-  //     /***
-  //      *
-  //      * Pass to SCIDB
-  //      *
-  //      */
-  //     ScidbLineType line;
-  //     line.push_back(dim_drug);
-  //     line.push_back(dim_adverse_effect);
-  //     line.push_back(is_adverse_effect);
-  //     coo->add(line);
-  //     ncell++;
-  //   }
+    fResChunk = fRes->Fetch();
+  }
+  cout << "resCnt: " << resCnt << endl;
 
-  //   conn->upload("temp",
-  //                coo);  // upload data to "testarray" array with coo format
+  t9UnpinBuffer(E->getArrayName());
 
-  //   conn->exec(
-  //       "store(redimension(temp, <is_adverse_effect:double NOT NULL>[drug=0:"
-  //       + to_string(dim1 - 1) + ":0:1000; adverse_effect=0:" + to_string(dim2
-  //       - 1) +
-  //       ":0:1000], false),  drug_matrix)");
-  //   conn->exec("store(spgemm(drug_matrix,transpose(drug_matrix)),similarity1)");
-  //   conn->exec(
-  //       "store(project(apply(filter(similarity1,drug=drug2),result,1/"
-  //       "sqrt(multiply)),result),inv_norm)");
-  //   conn->exec("store(spgemm(similarity1,inv_norm),similarity2)");
-  //   conn->exec("store(spgemm(transpose(similarity2),inv_norm),drug_similarity)");
-
-  //   mysql.mysess->sql("use Healthcare").execute();
-
-  //   std::string query =
-  //       "select distinct Rdrug.drug_d "
-  //       "from Prescription, Rdrug "
-  //       "where Rdrug.drug = Prescription.drug_id and Prescription.patient_id
-  //       = " + to_string(patient_id);
-
-  //   auto prescribed_drugs = mysql.mysess->sql(query).execute();
-
-  //   ScidbSchema schema;
-  //   schema.dims.push_back(ScidbDim("drug22", 0, INT32_MAX, 0, 1000000));
-  //   schema.attrs.push_back(ScidbAttr("multiply", DOUBLE));
-
-  //   // cout << "drug1\tdrug2\tsimilarity" << endl;
-  //   int nrow = 0;
-  //   for (auto row : prescribed_drugs) {
-  //     // cout << "---------drug_idx: " << row[0].get<int>() << "----------"
-  //     <<
-  //     // endl;
-  //     auto download = conn->download(
-  //         "slice(drug_similarity,drug2," + to_string(row[0].get<int>()) +
-  //         ")", schema);
-  //     auto line = download->readcell();
-  //     while (line.size() != 0) {
-  //       // cout << row[0].get<int>() << "\t";
-  //       // cout << get<int>(line.at(0)) << "\t";
-  //       // cout << get<double>(line.at(1)) << "\t";
-  //       // cout << endl;
-  //       line = download->readcell();
-  //       nrow++;
-  //     }
-  //   }
-
-  //   cout << "TASK9: TOTAL " << nrow++ << " ROWS ARE REPORTED" << endl;
+  cout << "[TASK9] DONE" << endl;
 }
--- Impl/polyglot/run_tasks/src/include/Polyglot/func.h
+++ Impl/polyglot/run_tasks/src/include/Polyglot/func.h
@@ -13,3 +13,11 @@ void t0ConstructX(duckdb::Connection &dconn, int personSize, int tagSize);
 void t0ConstructY(duckdb::Connection &dconn, int personSize,
                   int favoriteBrandId);
 void t2ConstructX(duckdb::Connection &dconn, int customerSize, int productSize);
+
+void t9_invnorm(Chunk &opnd, Chunk &result);
+void t9ConstructD(duckdb::Connection &dconn, int drugSize,
+                  int adverseEffectSize);
+PFpage *t9GetBuffer(string arrName);
+void t9UnpinBuffer(string arrName);
+std::vector<pair<int, double>> t9GetValues(duckdb::Connection &dconn,
+                                           PFpage *page, int id);
